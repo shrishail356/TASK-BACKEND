a872dcb5cf322c9d5f6121a96ad94cfc96d820e4
Shrishail Patil <91820178+shrishail356@users.noreply.github.com>
2024-02-08 23:03:33 +0530

    uploaded backend

---
+++ index.js
@@ -0,0 +1,16 @@
+const connectToMongo = require("./Database/db");
+const express = require("express");
+const app = express();
+connectToMongo();
+const port = 5000 || process.env.PORT;
+var cors = require("cors");
+
+app.use(cors());
+app.use(express.json()); 
+
+app.use("/api/admin/auth", require("./routes/Admin Api/adminCredential"));
+app.use("/api/admin/details", require("./routes/Admin Api/adminDetails"));
+app.use("/api/notice", require("./routes/notice"));
+app.listen(port, () => {
+  console.log(`Server Listening On http://localhost:${port}`);
+});
